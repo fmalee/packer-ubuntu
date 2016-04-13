16f9eac83515f067380f14e0e106184e762c021c
Joey Lee <lixueli@gmail.com>
2016-04-13 10:15:39 +0800

    将Shell命令改为严格的`-eux`模式。这样虽然降低制作镜像成功率，但能保证每一行脚本都得到有效执行，保证制作出来的Box都是达到设计要求的。 同时减少个别要卸载的包，以避免在`-eux`模式下出错： - 并不存在的包： `libffi5`,`doc`,`ubuntu-serverguide` - Nginx的依赖包：`libxcb1`,`libx11-data`,`libx11-6`

--- script/minimize.sh
+++ script/minimize.sh
@@ -24,15 +24,16 @@ echo "==> Removing development tools"
 #apt-get -y purge cpp gcc g++ 
 apt-get -y purge build-essential
 echo "==> Removing default system Ruby"
-apt-get -y purge ruby ri doc
+# apt-get -y purge ruby ri doc
+apt-get -y purge ruby ri
 echo "==> Removing default system Python"
-apt-get -y purge python-dbus libnl1 python-smartpm python-twisted-core libiw30 python-twisted-bin libdbus-glib-1-2 python-pexpect python-pycurl python-serial python-gobject python-pam python-openssl libffi5
+apt-get -y purge python-dbus libnl1 python-smartpm python-twisted-core libiw30 python-twisted-bin libdbus-glib-1-2 python-pexpect python-pycurl python-serial python-gobject python-pam python-openssl
 echo "==> Removing X11 libraries"
-apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6
+apt-get -y purge xauth libxmuu1 libxext6
 echo "==> Removing obsolete networking components"
 apt-get -y purge ppp pppconfig pppoeconf
 echo "==> Removing other oddities"
-apt-get -y purge popularity-contest installation-report landscape-common wireless-tools wpasupplicant ubuntu-serverguide
+apt-get -y purge popularity-contest installation-report landscape-common wireless-tools wpasupplicant
 
 # Clean up the apt cache
 apt-get -y autoremove --purge
