ff2a119aec3514cbf4b490fda33e3fb6d4e21e5b
Joey Lee <lixueli@gmail.com>
2016-04-17 10:28:47 +0800

    - 添加官方源以更新Nginx到最新版本(1.9+)，同时做一些基础配置修改 - 添加两种方式的官方源以更新MySQL到最新版本(5.7+)，同时做一些基础配置修改 - 添加一些PHP扩展，同时做一些基础配置修改 - 安装`nodes`和`ssl-cert` - 使用变量`SSH_USERNAME`统一控制用户名 - 去除`linux-headers-$(uname -r)`安装，在`preseed.cfg`中已经预安装 - 修正一些拼写问题

--- script/package.sh
+++ script/package.sh
@@ -1,20 +1,34 @@
 #!/bin/bash -eux
 
+SSH_USERNAME=${SSH_USERNAME:-vagrant}
 SSH_PASSWORD=${SSH_PASSWORD:-vagrant}
+MYSQL_APT=${MYSQL_APT:-0.7.2-1}
 
 if [[ $LNMP  =~ true || $LNMP =~ 1 || $LNMP =~ yes ]]; then
-    echo "==> Adding Apt Repository(Blackfire & PHP)"
+    echo "==> Adding Apt Repository(PHP & Nginx & MySQL & Blackfire)"
 	# 添加add-apt-repository命令
 	apt-get -y install software-properties-common
-	# Blackfire源
-	wget -O - https://packagecloud.io/gpg.key | sudo apt-key add -
-	echo "deb http://packages.blackfire.io/debian any main" | sudo tee /etc/apt/sources.list.d/blackfire.list
 	# PHP 7.0源
 	LC_ALL=en_US.UTF-8 add-apt-repository -y ppa:ondrej/php
 	LC_ALL=en_US.UTF-8 add-apt-repository -y ppa:ondrej/php-qa
+	# Nginx源
+	add-apt-repository -y ppa:nginx/development
+	# Mysql5.7源
+	# sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5072E1F5 
+	# echo "deb http://repo.mysql.com/apt/ubuntu/ precise mysql-5.7" | sudo tee /etc/apt/sources.list.d/mysql.list
+	sudo wget -P /tmp http://repo.mysql.com/mysql-apt-config_${MYSQL_APT}_all.deb
+	sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-router select none'
+	sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-connector-python select none'
+	sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-workbench select none'
+	sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-server select mysql-5.7'
+	sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-mysql-utilities select mysql-utilities-1.5'
+	sudo DEBIAN_FRONTEND=noninteractive dpkg -i /tmp/mysql-apt-config_${MYSQL_APT}_all.deb
+	# Blackfire源
+	wget -O - https://packagecloud.io/gpg.key | sudo apt-key add -
+	echo "deb http://packages.blackfire.io/debian any main" | sudo tee /etc/apt/sources.list.d/blackfire.list
 fi
 
-echo "==> Updating list of repositories"
+echo "==> Updating the package list"
 echo $UPDATE_PROXY
 if [[ $UPDATE_PROXY == false || $UPDATE_PROXY == 0 || $UPDATE_PROXY == no ]]; then
 	apt-get -y update
@@ -27,7 +41,6 @@ echo "==> Installing software(Include Packages)"
 # 安装语言包
 apt-get install -y language-pack-en-base language-pack-zh-hans
 # 安装之前'pkgsel/include'忽略的软件
-apt-get -y install linux-headers-$(uname -r)
 apt-get -y install build-essential gcc make
 apt-get -y install zlib1g-dev libssl-dev libreadline-dev libyaml-dev
 apt-get -y install cryptsetup
@@ -41,16 +54,40 @@ if [[ $LNMP  =~ true || $LNMP =~ 1 || $LNMP =~ yes ]]; then
 	echo "==> Installing LNMP & ZSH & Blackfire"
 
 	apt-get install -y php7.0 php7.0-fpm php7.0-common
-	apt-get install -y php7.0-gd php7.0-mysql php7.0-mcrypt php7.0-curl php7.0-intl php7.0-mbstring
-	apt-get install -y php7.0-zip php7.0-bcmath
-	# 设置PHP时区
-	sed -i.bak-timezone "s/;date.timezone =.*/date.timezone = PRC/" /etc/php/7.0/fpm/php.ini
+	apt-get install -y php7.0-opcache php7.0-gd php7.0-mysql php7.0-mcrypt php7.0-curl php7.0-intl php7.0-mbstring
+	apt-get install -y php7.0-zip php7.0-imap php7.0-sqlite3 php7.0-pgsql php7.0-soap php7.0-json php7.0-xml php7.0-bcmath
+	apt-get install -y php-xdebug
+	# PHP 7.0
+	sed -i "s/^[;]*date.timezone\([[:space:]]*\)=\([[:space:]]*\).*/date.timezone = PRC/g" /etc/php/7.0/fpm/php.ini
+	sed -i "s/^upload_max_filesize\([[:space:]]*\)=\([[:space:]]*\).*/upload_max_filesize = 100M/g" /etc/php/7.0/fpm/php.ini
+	sed -i "s/[;]*cgi.fix_pathinfo\([[:space:]]*\)=\([[:space:]]*\).*/cgi.fix_pathinfo = 0/g" /etc/php/7.0/fpm/php.ini
+	sed -i "s/^post_max_size\([[:space:]]*\)=\([[:space:]]*\).*/post_max_size = 100M/g" /etc/php/7.0/fpm/php.ini
+	sed -i "s/^display_errors\([[:space:]]*\)=\([[:space:]]*\).*/display_errors = On/g" /etc/php/7.0/fpm/php.ini
+	sed -i "s/^error_reporting\([[:space:]]*\)=\([[:space:]]*\).*/error_reporting = E_ALL/g" /etc/php/7.0/fpm/php.ini
+	sed -i "s/^memory_limit\([[:space:]]*\)=\([[:space:]]*\).*/memory_limit = 512M/g" /etc/php/7.0/fpm/php.ini
+
+	sed -i "s/[;]*date.timezone\([[:space:]]*\)=\([[:space:]]*\).*/date.timezone = PRC/g" /etc/php/7.0/cli/php.ini
+	sed -i "s/^display_errors\([[:space:]]*\)=\([[:space:]]*\).*/display_errors = On/g" /etc/php/7.0/cli/php.ini
+	sed -i "s/^error_reporting\([[:space:]]*\)=\([[:space:]]*\).*/error_reporting = E_ALL/g" /etc/php/7.0/cli/php.ini
+	sed -i "s/^memory_limit\([[:space:]]*\)=\([[:space:]]*\).*/memory_limit = 512M/g" /etc/php/7.0/cli/php.ini
+
+	sed -i "s/^user\([[:space:]]*\)=\([[:space:]]*\).*/user = $SSH_USERNAME/g" /etc/php/7.0/fpm/pool.d/www.conf
+	sed -i "s/^group\([[:space:]]*\)=\([[:space:]]*\).*/group = $SSH_USERNAME/g" /etc/php/7.0/fpm/pool.d/www.conf
+	sed -i "s/^listen.owner\([[:space:]]*\)=\([[:space:]]*\).*/listen.owner = $SSH_USERNAME/g" /etc/php/7.0/fpm/pool.d/www.conf
+	sed -i "s/^listen.group\([[:space:]]*\)=\([[:space:]]*\).*/listen.group = $SSH_USERNAME/g" /etc/php/7.0/fpm/pool.d/www.conf
+	sed -i "s/[;]*listen.mode\([[:space:]]*\)=\([[:space:]]*\).*/listen.mode = 0666/g" /etc/php/7.0/fpm/pool.d/www.conf
+
+	echo "==> Installing Nginx 1.9"
+	apt-get -y install nginx-full nginx
+	# Nginx full 1.9
+	sed -i "s/^user\([[:space:]]*\).*/user $SSH_USERNAME;/g" /etc/nginx/nginx.conf
+	sed -i "s/[# ]*server_names_hash_bucket_size\([[:space:]]*\).*/server_names_hash_bucket_size 64;/g" /etc/nginx/nginx.conf
 
 	echo "==> Installing other packages"
 	apt-get -y install memcached
-	# apt-get -y install redis-server
-	apt-get -y install nginx
-	apt-get -y install git rsync beanstalkd blackfire-agent
+	# apt-get -y install redis-server sqlite3 postgresql
+	apt-get -y install git nodejs rsync beanstalkd ssl-cert
+	apt-get -y install blackfire-agent blackfire-php
 
 	curl -sS https://getcomposer.org/installer | php
 	mv composer.phar /usr/local/bin/composer
@@ -61,19 +98,27 @@ if [[ $LNMP  =~ true || $LNMP =~ 1 || $LNMP =~ yes ]]; then
 	# 切换默认SHELL
 	sed -i.bak "s/\/bin\/bash/\/bin\/zsh/g" /etc/passwd
 
-	echo "==> Installing MySQL 5.6"
-	debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password password $SSH_PASSWORD"
-	debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password_again password $SSH_PASSWORD"
+	echo "==> Installing MySQL 5.7"
+	debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password $SSH_PASSWORD"
+	debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password $SSH_PASSWORD"
+	DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
 
-	apt-get -y install mysql-server-5.6 mysql-client-5.6
-
-	# MySQL远程连接
-	sed -i.bak-bind "s/^bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
-	mysql -uroot -p$SSH_PASSWORD -e "grant all on *.* to root@'%' identified by '$SSH_PASSWORD'";
-	mysql -uroot -p$SSH_PASSWORD -e "flush privileges";
-
-	# MySQL字符编码
-	sed -i.bak-utf8 "/\[client\]/a\default-character-set=utf8" /etc/mysql/my.cnf
+	# Configure MySQL default charset to real utf8
+	# sed  -i '/\[client\]/a default-character-set = utf8mb4' /etc/mysql/my.cnf
+	# sed  -i '/\[mysqld\]/a character-set-client-handshake = FALSE' /etc/mysql/my.cnf
+	# sed  -i '/\[mysqld\]/a character-set-server = utf8mb4' /etc/mysql/my.cnf
+	# sed  -i '/\[mysqld\]/a collation-server = utf8mb4_unicode_ci' /etc/mysql/my.cnf
+	sed -i "/\[client\]/a\default-character-set=utf8" /etc/mysql/my.cnf
 	sed -i "/\[mysqld\]/a\character-set-server=utf8" /etc/mysql/my.cnf
 	sed -i "/\[mysql\]/a\default-character-set=utf8" /etc/mysql/my.cnf
+
+	# Allow remote connections to MySQL server
+	sed -i "s/[# ]*bind-address\([[:space:]]*\)=\([[:space:]]*\).*/bind-address = 0.0.0.0/g" /etc/mysql/my.cnf
+	mysql -uroot -p$SSH_PASSWORD -e "grant all on *.* to root@'%' identified by '$SSH_USERNAME'";
+	mysql -uroot -p$SSH_PASSWORD -e "flush privileges";
+
+	# Loads timezone tables
+	mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql --user=root --password=$SSH_PASSWORD --force mysql
+
+	sed -i '$ a\'"default_password_lifetime = 0" /etc/mysql/my.cnf
 fi
